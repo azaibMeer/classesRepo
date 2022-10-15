c2a0c3c483d512df834651639207753b18c33864
azaibMeer <azaibmir1292@gmail.com>
2022-10-15 18:37:42 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,89 @@
-let AutoMobile = class {
-  constructor(name, model,color,price) {
-    this.name = name;
-    this.model = model;
-    this.color = color;
-    this.price = price;
-  }
-  start(){
-  	return "log car ready for drive";
-  }
-  openDoor(){
-  	return "log door is open";
-  }
-};
 
-class Car extends AutoMobile {
-  constructor(name, model,color,price) {
-    super(name, model,color,price);
-    this.name = name;
-    this.model = model;
-    this.color = color;
-    this.price = price;
-  }
-  
-}
-class Trucks extends AutoMobile {
-  constructor(name, model,color,price) {
-    super(name, model,color,price);
-    this.name = name;
-    this.model = model;
-    this.color = color;
-    this.price = price;
-  }
-  
-}
-class Bus extends AutoMobile {
-  constructor(name, model,color,price) {
-    super(name, model,color,price);
-    this.name = name;
-    this.model = model;
-    this.color = color;
-    this.price = price;
-  }
-  
-}
-let automobile = new AutoMobile("civic", 2017,"white","200000");
-let trucks = new AutoMobile("truck", 2027,"black","47850000");
- console.log(automobile.trucks.name);
+ class PersonAccount {
+     constructor(firstName, lastName, income, incomeDescription, expense, expenseDescription) {
+         this.firstName = firstName
+         this.lastName = lastName
+         this.incomes = [{
+             income,
+             incomeDescription
+         }]
+         this.expenses = [{
+             expense,
+             expenseDescription
+
+         }]
+     }
+
+     income(){
+        return console.log("income")
+     }
+     expense(){  
+         return console.log("expense")
+      }
+      balance(){
+         return console.log("balance")
+      }
+ }
+
+
+ const detail = new PersonAccount("azaib","mir",10000,"gud_income",5000,"Extra save");
+ console.log(detail);
+
+ 
+ class Automobile {
+     constructor(name, model, color, type) {
+         this.name = name
+         this.model = model
+         this.color = color
+         this.type = type
+     }
+     start() {
+         return console.log("Car is ready to drive")
+     }
+     openDoor() {
+         return console.log("Door is open")
+     }
+ }
+
+ class Car extends Automobile {
+     constructor(name, model, color, type,brand) {
+         super(name, model, color, type)
+         this.name = name
+         this.model = model
+         this.color = color
+         this.type = type
+         this.brand = brand
+         
+
+     }
+ }
+ class Bus extends Automobile {
+     constructor(name, model, color, type, brand) {
+         super(name, model, color, type)
+         this.name = name
+         this.model = model
+         this.color = color
+         this.type = type
+         this.brand = brand
+         
+
+     }
+ }
+
+ class Truck extends Automobile {
+     constructor(name, model, color, type, brand) {
+         super(name, model, color, type)
+         this.name = name
+         this.model = model
+         this.color = color
+         this.type = type
+         this.brand = brand
+         
+
+     }
+ }
+
+ const bus = new Bus("buss",2006,"red","w30","highfihgh");
+ console.log(bus)
+ bus.openDoor();
+ bus.start()
